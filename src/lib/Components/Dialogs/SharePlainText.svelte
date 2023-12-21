49216d7c62c3024461272bfd7285d8e258eb6c19
Erinell <ceddebois@gmail.com>
2023-12-21 09:40:03 +0100

    Added label configuration

--- src/lib/Components/Dialogs/SharePlainText.svelte
+++ src/lib/Components/Dialogs/SharePlainText.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import { Dialog, Button } from "$lib";
+  import { Dialog, Button, Label } from "$lib";
   import { Copy, Share2 } from "lucide-svelte";
   import { config } from "$lib/store";
   export let open = false;
@@ -12,11 +12,11 @@
   let code: any;
   let opened = false;
 
-  $:{
-    if (open && !opened){
+  $: {
+    if (open && !opened) {
       opened = true;
       code = JSON.stringify($config, null, 2);
-    } 
+    }
   }
 
   export let onClose = () => {};
@@ -28,7 +28,7 @@
       },
       function (err) {
         toastsStore.success(`${$_("unable-copy-config")}. ${err}`);
-      }
+      },
     );
   }
 </script>
@@ -40,7 +40,7 @@
     opened = open = false;
   }}
 >
-  <Dialog.Content class="w-4/5 h-3/4 flex flex-col">
+  <Dialog.Content class="max-w-max h-3/4 flex flex-col gap-0">
     <Dialog.Header>
       <Dialog.Title class="flex items-center">
         <Share2 class="w-6 mr-2" />{$_("share") +
@@ -48,18 +48,23 @@
           $_("plain-text")}</Dialog.Title
       >
     </Dialog.Header>
-    <div class="relative table h-full">
-      <div
-        class="h-full rounded-xl overflow-x-hidden overflow-y-auto hide-scrollbar"
-      >
-        <Highlight language={json} {code} />
+    <div class="flex h-full gap-2">
+      <div class="flex flex-col">
+        <Label class="m-2 mx-auto text-md">Configuration</Label>
+        <div class="relative table h-full">
+          <div
+            class="h-full rounded-xl overflow-x-hidden overflow-y-auto hide-scrollbar"
+          >
+            <Highlight language={json} {code} />
+          </div>
+          <Button
+            size="icon"
+            variant="outline"
+            class="w-9 h-9 absolute right-2 top-2"
+            on:click={copy}><Copy class="w-6 h-6" /></Button
+          >
+        </div>
       </div>
-      <Button
-        size="icon"
-        variant="outline"
-        class="w-9 h-9 absolute right-2 top-2"
-        on:click={copy}><Copy class="w-6 h-6" /></Button
-      >
     </div>
   </Dialog.Content>
 </Dialog.Root>
